73158be1253c6619fb6e2ea05696a3857b12e898
VakeDomen <vake.domen94@gmail.com>
2023-06-01 11:37:33 +0200

    basic pagination

--- src/models/query.rs
+++ src/models/query.rs
@@ -1,3 +1,6 @@
+use std::fmt::format;
+
+#[derive(Debug)]
 pub enum FilterOperator {
     Smaller,
     Bigger,
@@ -7,6 +10,7 @@ pub enum FilterOperator {
     Exists
 }
 
+#[derive(Debug)]
 pub enum Query {
     DataProviders(String),
     Discovery,
@@ -16,6 +20,7 @@ pub enum Query {
     Search(SearchType),
 }
 
+#[derive(Debug)]
 pub enum SearchType {
     Works(SearchQuery),
     Outputs(SearchQuery),
@@ -23,14 +28,28 @@ pub enum SearchType {
     Journals(SearchQuery),
 }
 
+#[derive(Debug)]
 pub struct SearchQuery {
-    filters: Vec<Filter>
+    filters: Vec<Filter>,
+    limit: Option<i32>,
+    offset: Option<i32>,
+    scroll: Option<bool>,
+    stats: Option<bool>
 }
 
+#[derive(Debug)]
 enum LogicalOperator {
     And,
     Or,
 }
+
+#[derive(Debug)]
+pub enum QueryRequestType {
+    Get,
+    Post,
+}
+
+#[derive(Debug)]
 struct Filter {
     logical_operator: LogicalOperator,
     filter_operator: FilterOperator,
@@ -38,9 +57,50 @@ struct Filter {
     value: Option<String>,
 }
 
+impl LogicalOperator {
+    fn parse(self) -> String {
+        match self {
+            LogicalOperator::And => "%20AND%20".to_string(),
+            LogicalOperator::Or => "%20OR%20".to_string(),
+        }
+    }
+}
+
+impl Filter {
+    fn parse(self) -> (LogicalOperator, String) {
+        let filter_query = match self.filter_operator {
+            FilterOperator::Smaller => format!("{}<{}", self.key, self.value.unwrap_or("".to_string())),
+            FilterOperator::Bigger => format!("{}>{}", self.key, self.value.unwrap_or("".to_string())),
+            FilterOperator::Eq => format!("{}={}", self.key, self.value.unwrap_or("".to_string())),
+            FilterOperator::SmallerEq => format!("{}<={}", self.key, self.value.unwrap_or("".to_string())),
+            FilterOperator::BiggerEq => format!("{}>={}", self.key, self.value.unwrap_or("".to_string())),
+            FilterOperator::Exists => format!("exists={}", self.key),
+        };
+        (self.logical_operator, filter_query)
+    }
+}
+
+impl SearchQuery {
+    pub fn paged(limit: i32, offset: i32) -> Self {
+        Self { 
+            filters: Default::default(), 
+            limit: Some(limit), 
+            offset: Some(offset), 
+            scroll: None, 
+            stats: None ,
+        }
+    }
+}
+
 impl Default for SearchQuery {
     fn default() -> Self {
-        Self { filters: Default::default() }
+        Self { 
+            filters: Default::default(), 
+            limit: None, 
+            offset: None, 
+            scroll: None, 
+            stats: None ,
+        }
     }
 }
 
@@ -63,12 +123,38 @@ impl SearchQuery {
         });
         self
     }
+    fn parse(self) -> String {
+        let mut final_filter = "?".to_string();
+        if let Some(l) = self.limit {
+            final_filter = format!("{}limit={}", final_filter, l);
+        }
+        if let Some(o) = self.offset {
+            final_filter = format!("{}&offset={}", final_filter, o);
+        }
+        if let Some(s) = self.scroll {
+            final_filter = format!("{}&scroll={}", final_filter, s);
+        }
+        if let Some(s) = self.stats {
+            final_filter = format!("{}&stats={}", final_filter, s);
+        }
+
+        if self.filters.len() < 1 {
+            return final_filter
+        } else {
+            final_filter = format!("{}&q=", final_filter);
+        }
+        for filter in self.filters.into_iter() {
+            let (op, query) = filter.parse();
+            if final_filter.eq("?") {
+                final_filter = format!("{}{}", final_filter, query);
+            } else {
+                final_filter = format!("{}{}{}", final_filter, op.parse(), query)
+            }
+        }
+        final_filter
+    }
 }
 
-pub enum QueryRequestType {
-    Get,
-    Post,
-}
 
 impl Query {
     pub fn request(self) -> (QueryRequestType, String) {
@@ -90,5 +176,6 @@ fn parse_search_query(st: SearchType) -> String {
         SearchType::DataProviders(sq) => ("data-providers".to_string(), sq),
         SearchType::Journals(sq) => ("journals".to_string(), sq),
     };
-    "search".to_string()
+    let query_string = query.parse();
+    format!("search/{}/{}", route, query_string)
 }
