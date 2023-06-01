ce05be81f8d0b2bf3a48f18076b41e1fc39768f9
VakeDomen <vake.domen94@gmail.com>
2023-06-01 11:37:33 +0200

    basic pagination

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,17 @@ use core_api_rs::{ApiBuilder, Query, SearchType, SearchQuery, FilterOperator, Wo
 
 fn main() {
     let mut api = ApiBuilder::set_key("DCrZJjaUtFd1KHg3zqbRTYelO9Xs26IM".to_string());
-    let setup_query = SearchQuery::default()
+    let setup_query = SearchQuery::paged(3, 1)
         .and(
             FilterOperator::Exists, 
             "doi".to_string(), 
             None
-        )
-        .and(
-            FilterOperator::Eq, 
-            "limit".to_string(),
-            Some("1".to_string())
         );
+
     let query = Query::Search(SearchType::Works(setup_query));
-    let resp = api.execute_query::<Work>(query);
+
+    let resp = query.request();
+
+    // let resp = api.execute_query::<Work>(query);
     println!("{:#?}", resp);
 }
