fdef6792b03ad449b1e1201931acf7a3573864ab
VakeDomen <vake.domen94@gmail.com>
2023-06-07 10:41:21 +0200

    doc comments to work attributes

--- src/models/work_models/data_provider.rs
+++ src/models/work_models/data_provider.rs
@@ -3,8 +3,15 @@ use serde::{Deserialize, Serialize};
 /// struct holds the avalible data on the [data providers](https://api.core.ac.uk/docs/v3#tag/Data-Providers)
 #[derive(Debug, Deserialize, Serialize)]
 pub struct DataProvider {
+    /// ID of the data provider.
     pub id: i32,
+
+    /// Name of the data provider.
     pub name: String,
+
+    /// URL of the data provider.
     pub url: String,
+
+    /// Logo of the data provider.
     pub logo: String,
 }
