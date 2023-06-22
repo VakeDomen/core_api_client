21e2c109529702466ad4ce2e5fa8e9dd0b481569
VakeDomen <vake.domen94@gmail.com>
2023-06-22 11:57:41 +0200

    abstracted query build into api method

--- src/models/api.rs
+++ src/models/api.rs
@@ -16,11 +16,121 @@ use super::query::Query;
 pub struct Api {
     key: String,
     client: Client,
+    log_target: bool,
+    log_raw_response: bool,
 }
 
 
 impl Api {
-    pub fn execute_query<T1, T2>(
+
+    /// Executes a search on the API for works based on the query.
+    /// 
+    /// ```
+    /// use core_api_rs::FilterOperator;
+    /// use core_api_rs::Api;
+    /// 
+    /// let api = Api::from("API_KEY");
+    /// 
+    /// let query = api.paged_search(10, 0)
+    ///    .and(FilterOperator::Exists("doi"))
+    ///    .and(FilterOperator::Bigger("citationCount", 20));
+    /// 
+    /// let resp = api.search_works(query);
+    /// ```
+    /// 
+    pub fn search_works<T1, T2>(
+        &self, 
+        query: SearchQuery<T1, T2>
+    ) -> Result<ApiResponse, crate::errors::Error> 
+    where 
+        T1: ToString + Clone,
+        T2: ToString + Clone,
+    {
+        self.execute_query(Query::SearchWorks(query))
+    }
+    
+    /// Executes a search on the API for works based on the query.
+    /// 
+    /// ```
+    /// use core_api_rs::FilterOperator;
+    /// use core_api_rs::Api;
+    /// 
+    /// let api = Api::from("API_KEY");
+    /// 
+    /// let query = api.paged_search(10, 0)
+    ///    .and(FilterOperator::Exists("software"))
+    ///    .and(FilterOperator::HasValue("type", "JOURNAL"));
+    /// let resp = api.search_data_providers(query);
+    /// ```
+    /// 
+    pub fn search_data_providers<T1, T2>(
+        &self, 
+        query: SearchQuery<T1, T2>
+    ) -> Result<ApiResponse, crate::errors::Error> 
+    where 
+        T1: ToString + Clone,
+        T2: ToString + Clone,
+    {
+        self.execute_query(Query::SearchDataProviders(query))
+    }
+
+    pub fn search_journals<T1, T2>(
+        &self, 
+        query: SearchQuery<T1, T2>
+    ) -> Result<ApiResponse, crate::errors::Error> 
+    where 
+        T1: ToString + Clone,
+        T2: ToString + Clone,
+    {
+        self.execute_query(Query::SearchJournals(query))
+    }
+
+    pub fn search_outputs<T1, T2>(
+        &self, 
+        query: SearchQuery<T1, T2>
+    ) -> Result<ApiResponse, crate::errors::Error> 
+    where 
+        T1: ToString + Clone,
+        T2: ToString + Clone,
+    {
+        self.execute_query(Query::SearchOutputs(query))
+    }
+    
+
+    
+    pub fn paged_search<T1, T2>(
+        &self, limit: i32, 
+        offset: i32
+    ) -> SearchQuery<T1, T2> 
+    where 
+        T1:ToString, 
+        T2:ToString 
+    {
+        SearchQuery::paged(limit, offset)
+    }
+
+    /// Method allows the user to override the default (flase) logging of the target URI that is being fetched
+    /// for data retrieval from the api
+    /// ```
+    /// use core_api_rs::Api;
+    /// let api = Api::from("API_KEY").log_target(true);
+    /// ```
+    pub fn log_target(self, log_target: bool) -> Self {
+        Self { key: self.key, client: self.client, log_target, log_raw_response: self.log_raw_response }
+    }
+
+
+    /// Method allows the user to override the default (flase) logging of the raw responses that are returned
+    /// from the API. 
+    /// ```
+    /// use core_api_rs::Api;
+    /// let api = Api::from("API_KEY").log_raw_response(true);
+    /// ```
+    pub fn log_raw_response(self, log_raw_response: bool) -> Self {
+        Self { key: self.key, client: self.client, log_target: self.log_target, log_raw_response }
+    }
+
+    fn execute_query<T1, T2>(
         &self, 
         query: Query<T1, T2>
     ) -> Result<ApiResponse, crate::errors::Error> 
@@ -30,6 +140,9 @@ impl Api {
     {
         let query_retained = query.clone();
         let (req_type, query_uri) = query.parse_request();
+        if self.log_target {
+            println!("{}", query_uri);
+        }
         let uri = format!("https://api.core.ac.uk/v3/{}", query_uri);
 
         let client_builer = match req_type {
@@ -43,7 +156,9 @@ impl Api {
                 Err(e) => return Err(crate::errors::Error::RequestError(e)),
             };
         let (data, rate_limit) = parse_raw_response(response)?;
-        println!("{:#?}", data);
+        if self.log_raw_response {
+            println!("{}", data);
+        }
         let deserialized_response = match query_retained {
             Query::DataProviders(_) => ApiResponseType::DataProviders(parse_json(&data)?),
             Query::Discovery => ApiResponseType::Discovery(parse_json(&data)?),
@@ -62,15 +177,12 @@ impl Api {
         })
     }
 
-    pub fn paged_search<T>(&self, limit: i32, offset: i32) -> SearchQuery<String, T> where T:ToString {
-        SearchQuery::paged(limit, offset)
-    }
 }
 
 impl<T: Into<String>> From<T> for Api {
     fn from(key: T) -> Self {
         let client = reqwest::blocking::Client::new();
-        Api { key: key.into(), client }
+        Api { key: key.into(), client, log_target: false, log_raw_response: false }
     }
 }
 
